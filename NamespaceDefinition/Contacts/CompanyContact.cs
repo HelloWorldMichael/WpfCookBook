a41dda28f186396b02afcc5a20e1666e990ecb7b
Michael <helloworld.michael@hotmail.com>
2014-02-28 16:41:05 +0800

    Add all the files

---
+++ NamespaceDefinition/Contacts/CompanyContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceDefinition.Contacts
+{
+	public class CompanyContact
+	{
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public int Age { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("Name: {0}\nPhone Number: {1}\nAge: {2}", Name, PhoneNumber, Age);
+		}
+	}
+}
