50aa9711b4ba043baa958e0169b43f9f9eb031f7
Michael <helloworld.michael@hotmail.com>
2014-03-13 23:06:18 +0800

    Add ContextMenu

--- StandardControls/MainWindow.xaml.cs
+++ StandardControls/MainWindow.xaml.cs
@@ -29,5 +29,38 @@ namespace StandardControls
 		{
 			//MessageBox.Show(string.Format("User: {0}, Comment:{1}{2}", _name.Text, Environment.NewLine, _comment.Text));
 		}
+
+		private void OnLangChanged(object sender, SelectionChangedEventArgs e)
+		{
+			_keywordList.Items.Clear();
+			string[] keywords = null;
+
+			switch (_langCombo.SelectedIndex)
+			{
+				case 0: // C++
+					keywords = new string[] { "for", "auto", "mutable", "explicit", "class", "volatile" };
+					break;
+				case 1: // C#
+					keywords = new string[] { "while", "var", "implicit", "return", "where", "enum" };
+					break;
+				case 2: // VB
+					keywords = new string[] { "Dim", "Select", "While", "Property", "Function", "If" };
+					break;
+				case 3: // F#
+					keywords = new string[] { "let", "rec", "mutable", "module", "yield", "type" };
+					break;
+			}
+			if (keywords != null)
+			{
+				Array.ForEach(keywords, keyword => _keywordList.Items.Add(keyword));
+			}
+		}
+
+		private void OnChangeLanugage(object sender, RoutedEventArgs e)
+		{
+			var item = e.Source as MenuItem;
+			_langCombo.SelectedIndex = Convert.ToInt32(item.Tag);
+		}
 	}
+
 }
