a2a5f722286d03e87d1ec6aed1095883d9598bfa
Michael <helloworld.michael@hotmail.com>
2014-03-16 22:50:13 +0800

    Added data binding (1)

---
+++ NamespaceDefinition/Contacts/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace NamespaceDefinition.Contacts
+{
+	public class Person : ObservableObject
+	{
+		public string Name { get; set; }
+		private int _age;
+		public int Age
+		{
+			get
+			{ return _age; }
+			set
+			{
+				_age = value;
+				OnPropertyChanged("Age");
+			}
+		}
+	}
+}
---
+++ NamespaceDefinition/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace NamespaceDefinition
+{
+	public abstract class ObservableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propName)
+		{
+			var pc = PropertyChanged;
+			if (pc != null)
+				pc(this, new PropertyChangedEventArgs(propName));
+		}
+	}
+}
